0931d0fc510dc33764ad47c3b13be6941d4888dc
project516 <138796702+Project516@users.noreply.github.com>
2026-04-23 14:03:51 +0000

    create calculator program

--- src/main.c
+++ src/main.c
@@ -3,9 +3,49 @@
 int main(void)
 {
     float first, second, result;
+    char op;
     
     printf("This is a simple calculator in c\n");
+    
     printf("Enter your first number: ");
-
     scanf("%f", &first);
+
+    printf("Enter operator: ");
+    scanf(" %c", &op);
+
+    printf("Enter your second number: ");
+    scanf("%f", &second);
+
+    switch(op)
+    {
+        case '+': 
+            result = first + second;
+            break;
+        case '-': 
+            result = first - second;
+            break;
+        case '*': 
+            result = first * second;
+            break;
+        case '/': 
+            if (second == 0) 
+            {
+                printf("Divide by zero error\n");
+                break;
+            }
+            result = first / second;
+            break;
+        case '%': 
+            int whole_first = first;
+            int whole_second = second;
+            result = whole_first % whole_second;
+            break;
+        default:
+            printf("Unknown operator\n");
+            break;
+    }
+
+    printf("Result: %f\n", result);
+
+    return 0;
 }
