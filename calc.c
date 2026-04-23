ee477f19abd9a329b7d69c1c4f64e8eaa2f0143b
project516 <138796702+Project516@users.noreply.github.com>
2026-04-22 19:17:14 -0500

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float first, second, result;
+    
+    printf("This is a simple calculator in c\n");
+    printf("Enter your first number: ");
+
+    scanf("%f", &first);
+
+
+}
